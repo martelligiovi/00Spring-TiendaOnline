e94cde5a0dda9d441dc670a2faa3b644ebd14551
martelligiovi <martelligiovi@gmail.com>
2023-07-17 18:41:40 -0300

    implementacion de persona

---
+++ tienda/src/main/java/edu/EscuelaIT/tienda/controllers/PersonaRestController.java
@@ -0,0 +1,30 @@
+package edu.EscuelaIT.tienda.controllers;
+
+import edu.EscuelaIT.tienda.domain.Persona;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PersonaRestController {
+
+     public Persona getPersonaPorId(Long id) {
+         return null;
+     }
+     public List<Persona> listPersona() {
+         return null;
+     }
+     public Persona createPersona(Persona persona) {
+         return null;
+     }
+     public Persona updatePersona(Persona persona) {
+         return null;
+     }
+     public void deletePersona(Long id) {
+
+     }
+
+
+
+
+}
---
+++ tienda/src/main/java/edu/EscuelaIT/tienda/domain/Persona.java
@@ -0,0 +1,11 @@
+package edu.EscuelaIT.tienda.domain;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+    private String nombre;
+    private Long id;
+    private String apellido;
+
+}
